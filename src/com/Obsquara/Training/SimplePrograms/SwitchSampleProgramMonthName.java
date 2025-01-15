3b8bd04757f7645e5316818703eba79b765502e9
treas <treas@Stevu>
2025-01-15 15:15:25 +0530

    Switch program committed

---
+++ src/com/Obsquara/Training/SimplePrograms/SwitchSampleProgramMonthName.java
@@ -0,0 +1,59 @@
+package com.Obsquara.Training.SimplePrograms;
+
+public class SwitchSampleProgramMonthName {
+
+
+	public static void main(String[] args) {
+		int num = 7;
+		switch(num)
+		{
+			case 1 :
+			{
+				System.out.println("1-Month-January");
+				break;	
+		}
+			case 2 :
+			{
+				System.out.println("2-Month-February");
+				break;	
+		}	
+			case 3 :
+			{
+				System.out.println("3-Month-March");
+				break;	
+		}
+			case 4 :
+			{
+				System.out.println("4-Month-April");
+				break;	
+		}
+			case 5 :
+			{
+				System.out.println("5-Month-May");
+				break;	
+		}
+			case 6 :
+			{
+				System.out.println("6-Month-June");
+				break;	
+		}
+			case 7 :
+			{
+				System.out.println("7-Month-July");
+				break;	
+		}
+			case 8 :
+			{
+				System.out.println("8-Month-July");
+				break;	
+		}
+			default :
+			{
+				System.out.println("Not match");
+			}
+		}
+		
+
+	}
+
+}
