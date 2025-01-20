7217494a88bdd02206aca3c6a8f6226dfe2f4a88
treas <treas@Stevu>
2025-01-20 12:34:39 +0530

    Corrected program

--- src/com/Obsquara/Training/Inheritance/AdditionResult.java
+++ src/com/Obsquara/Training/Inheritance/AdditionResult.java
@@ -1,29 +1,28 @@
 package com.Obsquara.Training.Inheritance;
 
 public class AdditionResult extends AddNumbers {
-	public int add(int a,int b)
+	public void div()
 	{
-		int val = super.add(50,1);
-		return(val);
-			
+	int v = super.add(50,50);
+	System.out.println("Addition result : " + v);
+	int res = v%10;
+	System.out.println("Reminder when divided by 10 : " +res);
+	if(res==0)
+	{
+		System.out.println("Result divisible by 10");
 	}
+	else
+	{
+		System.out.println("Result not divisible by 10");
+	}
+}
 
 	public static void main(String[] args) {
 		
 
 		AdditionResult obj = new AdditionResult();
-		int v = obj.add(5, 5);
-		System.out.println("Addition result : " + v);
-		int res = v%10;
-		System.out.println("Reminder when divided by 10 : " +res);
-		if(res==0)
-		{
-			System.out.println("Result divisible by 10");
-		}
-		else
-		{
-			System.out.println("Result not divisible by 10");
-		}
+		obj.div();
+		
 		
 
 	}
